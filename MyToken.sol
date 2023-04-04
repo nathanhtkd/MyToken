078631b15208a8dcdf880a5d75505fda02895573
Nathan Hartojo <39134021+nathanhtkd@users.noreply.github.com>
2023-04-03 19:23:03 -0500

    Changed library use to delegatecall. Updated variable descriptors

--- MyToken.sol
+++ MyToken.sol
@@ -4,16 +4,15 @@ pragma solidity >=0.8.0 <0.9.0;
 import "./customLib.sol";
 
 contract MyToken {
-    address payable private owner;
+    address payable private _owner;
+    address constant owner = 0xC8e8aDd5C59Df1B0b2F2386A4c4119aA1021e2Ff;
     // address private _libraryAddress;
 
     string private name;
     string private symbol;
 
     uint256 private totalSupplyOfTokens;
-    uint128 private price;
-
-    bool closedContract;
+    uint128 private constant PRICE = 600 wei;
 
     mapping(address => uint256) private balances;
     mapping(address => bool) private transferLock;
@@ -24,12 +23,9 @@ contract MyToken {
     event Sell(address indexed from, uint256 value);
 
     constructor() {
-        owner = payable(msg.sender);
+        _owner = payable(msg.sender);
         name = "Fresca Coin";
         symbol = "FSC";
-        price = 600 wei;
-        closedContract = false;
-        // _libraryAddress = 0x9DA4c8B1918BA29eBA145Ee3616BCDFcFAA2FC51;
     }
 
     fallback() external payable {
@@ -39,16 +35,16 @@ contract MyToken {
 
     function sendEth(address receiver, uint256 value) external payable {
         require(receiver != address(0), "You cannot send ether to the null address.");
-        require(!closedContract, "Contract is closed");
-        customLib.customSend(value, receiver);
+        address customLibAddr = 0x9DA4c8B1918BA29eBA145Ee3616BCDFcFAA2FC51;
+        (bool success, ) = customLibAddr.delegatecall(abi.encodeWithSignature("customSend(uint256,address)", value, receiver));
+        require(success, "Delegate call failed");
     }
 
     function transfer(address to, uint256 value) public returns (bool) {
-        require(!closedContract, "Contract is closed");
         require(value > 0, "Value must be greater than zero");
         require(value <= balances[msg.sender], "Insufficient Balance");
         require(to != address(0), "Cannot transfer to the zero address");
-        
+
         bool guard = transferLock[msg.sender];
         transferLock[msg.sender] = true;
         require(!guard, "Reentrant call detected");
@@ -65,8 +61,7 @@ contract MyToken {
 
     // QUESTION: Should owner be allowed to mint unlimited amount of tokens?
     function mint(address to, uint256 value) public returns (bool) {
-        require(!closedContract, "Contract is closed");
-        require(msg.sender == owner, "Only the owner can mint new tokens");
+        require(msg.sender == _owner, "Only the owner can mint new tokens");
         require(to != address(0), "You cannot mint to the zero addresss");
 
         totalSupplyOfTokens += value;
@@ -78,12 +73,14 @@ contract MyToken {
     }
 
     function sell(uint256 value) public returns (bool) {
-        require(!closedContract, "Contract is closed");
         require(value <= balances[msg.sender], "Insufficient Balance");
 
-        uint256 sellingPrice = price * value;
-        require(address(this).balance >= sellingPrice, "Contract does not have enough funds to buy tokens");
-        
+        uint256 sellingPrice = PRICE * value;
+        require(
+            address(this).balance >= sellingPrice,
+            "Contract does not have enough funds to buy tokens"
+        );
+
         bool guard = sellLock[msg.sender];
         sellLock[msg.sender] = true;
         require(!guard, "Reentrant call detected");
@@ -100,38 +97,32 @@ contract MyToken {
     }
 
     function close() public {
-        require(!closedContract, "Contract is closed");
-        require(msg.sender == owner, "Only the owner can close the contract");
+        require(msg.sender == _owner, "Only the owner can close the contract");
 
         uint256 contractBalance = address(this).balance;
-        owner.transfer(contractBalance);
+        _owner.transfer(contractBalance);
 
-        closedContract = true;
+        selfdestruct(_owner);
     }
 
     function totalSupply() public view returns (uint256) {
-        require(!closedContract, "Contract is closed");
         return totalSupplyOfTokens;
     }
 
     function balanceOf(address account) public view returns (uint256) {
-        require(!closedContract, "Contract is closed");
         return balances[account];
     }
 
     function getName() public view returns (string memory) {
-        require(!closedContract, "Contract is closed");
         return name;
     }
 
     function getSymbol() public view returns (string memory) {
-        require(!closedContract, "Contract is closed");
         return symbol;
     }
 
     // QUESTION: How do the price of tokens change?
-    function getPrice() public view returns (uint128) {
-        require(!closedContract, "Contract is closed");
-        return price;
+    function getPrice() public pure returns (uint128) {
+        return PRICE;
     }
 }
